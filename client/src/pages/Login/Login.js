07e5b31072c0295b19b195f813b1c365655fb01d
Aditya Nair <adinair.in@gmail.com>
2023-03-11 15:14:30 -0600

    Updated Login Page

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,12 +1,14 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { useMutation } from '@apollo/client';
-import { LOGIN_USER } from '../../utils/mutations';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { useMutation } from "@apollo/client";
+import { LOGIN_USER } from "../../utils/mutations";
+import "./Login.css";
+import MojaveLogo from "../../assets/MojaveLogo2.png";
 
-import Auth from '../../utils/auth';
+import Auth from "../../utils/auth";
 
 const Login = (props) => {
-  const [formState, setFormState] = useState({ email: '', password: '' });
+  const [formState, setFormState] = useState({ email: "", password: "" });
   const [login, { error, data }] = useMutation(LOGIN_USER);
 
   // update state based on form input changes
@@ -35,60 +37,109 @@ const Login = (props) => {
 
     // clear form values
     setFormState({
-      email: '',
-      password: '',
+      email: "",
+      password: "",
     });
   };
 
   return (
-    <main className="flex-row justify-center mb-4">
-      <div className="col-12 col-lg-10">
-        <div className="card">
-          <h4 className="card-header bg-dark text-light p-2">Login</h4>
-          <div className="card-body">
-            {data ? (
-              <p>
-                Success! You may now head{' '}
-                <Link to="/">back to the homepage.</Link>
-              </p>
-            ) : (
-              <form onSubmit={handleFormSubmit}>
-                <input
-                  className="form-input"
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <input
-                  className="form-input"
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <button
-                  className="btn btn-block btn-primary"
-                  style={{ cursor: 'pointer' }}
-                  type="submit"
-                >
-                  Submit
-                </button>
-              </form>
-            )}
-
-            {error && (
-              <div className="my-3 p-3 bg-danger text-white">
-                {error.message}
-              </div>
-            )}
-          </div>
-        </div>
+    <div className="login">
+      <Link to="/">
+        <img className="login_logo" src={MojaveLogo} />
+      </Link>
+      <div className="login_container">
+        <h1> Sign In </h1>
+        <form onSubmit={handleFormSubmit}>
+          <h5>E-mail</h5>
+          <input
+            className="form-input"
+            placeholder="Your email"
+            name="email"
+            type="email"
+            value={formState.email}
+            onChange={handleChange}
+          />
+          <h5>Password</h5>
+          <input
+            className="form-input"
+            placeholder="******"
+            name="password"
+            type="password"
+            value={formState.password}
+            onChange={handleChange}
+          />
+          <button
+            className="signIn_button"
+            style={{ cursor: "pointer" }}
+            type="submit"
+          >
+            Sign In
+          </button>
+        </form>
+        <p>
+          By sigining-in, you agree to MOJAVE's Terms and Conditions of Use &
+          Sale.
+        </p>
+        <Link to="/signup">
+          <button
+            className="create_button"
+            style={{ cursor: "pointer" }}
+            type="submit"
+          >
+            Create your MOJAVE account
+          </button>
+        </Link>
       </div>
-    </main>
+    </div>
   );
 };
 
 export default Login;
+
+// <main className="flex-row justify-center mb-4">
+    //   <div className="col-12 col-lg-10">
+    //     <div className="card">
+    //       <h4 className="card-header bg-dark text-light p-2">Login</h4>
+    //       <div className="card-body">
+    //         {data ? (
+    //           <p>
+    //             Success! You may now head{' '}
+    //             <Link to="/">back to the homepage.</Link>
+    //           </p>
+    //         ) : (
+    //           <form onSubmit={handleFormSubmit}>
+    //             <input
+    //               className="form-input"
+    //               placeholder="Your email"
+    //               name="email"
+    //               type="email"
+    //               value={formState.email}
+    //               onChange={handleChange}
+    //             />
+    //             <input
+    //               className="form-input"
+    //               placeholder="******"
+    //               name="password"
+    //               type="password"
+    //               value={formState.password}
+    //               onChange={handleChange}
+    //             />
+    //             <button
+    //               className="btn btn-block btn-primary"
+    //               style={{ cursor: 'pointer' }}
+    //               type="submit"
+    //             >
+    //               Submit
+    //             </button>
+    //           </form>
+    //         )}
+
+    //         {error && (
+    //           <div className="error_message">
+    //             {error.message}
+    //           </div>
+    //         )}
+    //       </div>
+    //     </div>
+    //   </div>
+    // </main>
