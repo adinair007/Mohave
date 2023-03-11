5363feea8e05e2536ea2d81046d598789ed79d19
Aditya Nair <adinair.in@gmail.com>
2023-03-10 22:05:12 -0600

    Added homepage component

---
+++ client/src/pages/Home.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import Desert from '../assets/Desert.jpg'
+
+function Home() {
+  return (
+    <div className="home">
+        <div className='home_container'>
+           <img className='home_image' src= {Desert} alt="Desert art"/>
+        </div>
+        <div className='home_row'>
+            
+        </div>
+        <div className='home_row'>
+            
+        </div>
+        <div className='home_row'>
+            
+        </div>
+    </div>
+  )
+}
+
+export default Home
