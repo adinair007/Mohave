93499b9dc5276a2fd739ca733cd8c3a67c282d73
Aditya Nair <adinair.in@gmail.com>
2023-03-11 15:20:05 -0600

    Updated Footer.css

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -8,11 +8,11 @@ const Footer = () => {
   return (
     <footer className="footer">
       <div className="container text-center mb-5">
-        {location.pathname !== "/" && (
+        {/* {location.pathname !== "/" && (
           <button className="btn btn-dark mb-3" onClick={() => navigate(-1)}>
             &larr; Go Back
           </button>
-        )}
+        )} */}
         <h4>Made by the Mojave Team 🐍.</h4>
       </div>
     </footer>
