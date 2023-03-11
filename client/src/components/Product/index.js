7f70140c6730dc455dd51cea41be527fdfdbd948
1jessep <jessepstynchcomb@gmail.com>
2023-03-11 11:26:21 -0600

    created StateProvider.js and reducer.js. edited Product.js. Working on adding Items to basket

--- client/src/components/Product/index.js
+++ client/src/components/Product/index.js
@@ -1,6 +1,21 @@
 import React from "react";
+import { useStateValue } from "../../StateProvider";
 
 function Product({ id, title, price, image, rating }) {
+  const [{ state }, dispatch] = useStateValue();
+  const addToBasket = () => {
+    //dispatch the item into basket/data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
   return (
     <div className="product">
       <div className="product_info">
@@ -18,7 +33,7 @@ function Product({ id, title, price, image, rating }) {
         </div>
       </div>
       <img src={image} />
-      <button>Add to Cart</button>
+      <button onClick={addToBasket}>Add to Cart</button>
     </div>
   );
 }
