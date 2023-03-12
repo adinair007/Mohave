19155f3de1b6161e7bcf6fc266597b065c83e17d
Aditya Nair <adinair.in@gmail.com>
2023-03-11 20:07:40 -0600

    Creating orders page

--- client/src/pages/Orders/Orders.js
+++ client/src/pages/Orders/Orders.js
@@ -2,11 +2,11 @@ import React from 'react';
 import { Navigate, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
 
-import { QUERY_USER, QUERY_ME } from '../utils/queries';
+import { QUERY_USER, QUERY_ME } from '../../utils/queries';
 
-import Auth from '../utils/auth';
+import Auth from '../../utils/auth';
 
-const Profile = () => {
+const Orders = () => {
   const { username: userParam } = useParams();
 
   const { loading, data } = useQuery(userParam ? QUERY_USER : QUERY_ME, {
@@ -36,7 +36,7 @@ const Profile = () => {
     <div>
       <div className="flex-row justify-center mb-3">
         <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} profile.
+          Viewing {userParam ? `${user.username}'s` : 'your'} orders.
         </h2>
 
         <div className="col-12 col-md-10 mb-5">
@@ -53,4 +53,4 @@ const Profile = () => {
   );
 };
 
-export default Profile;
+export default Orders;
