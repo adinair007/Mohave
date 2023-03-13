9ef0f70e203fef456265df69580c23b7934f8357
Aditya Nair <adinair.in@gmail.com>
2023-03-13 12:41:44 -0500

    Updated User model and signup page

--- client/src/pages/Orders/Orders.js
+++ client/src/pages/Orders/Orders.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { Navigate, useParams } from 'react-router-dom';
-import { useQuery } from '@apollo/client';
+import React from "react";
+import { Navigate, useParams } from "react-router-dom";
+import { useQuery } from "@apollo/client";
 
-import { QUERY_USER, QUERY_ME } from '../../utils/queries';
+import { QUERY_USER, QUERY_ME } from "../../utils/queries";
 
-import Auth from '../../utils/auth';
-import Header from '../../components/Header';
+import Auth from "../../utils/auth";
+import Header from "../../components/Header";
+import { keys } from "@material-ui/core/styles/createBreakpoints";
 
 const Orders = () => {
   const { username: userParam } = useParams();
@@ -37,19 +38,20 @@ const Orders = () => {
     <div>
       <Header />
       <div className="orders">
-        <h2 className="col-12 col-md-10 bg-dark text-light p-3 mb-5">
-          Viewing {userParam ? `${user.username}'s` : 'your'} orders.
-        </h2>
-
-        <div className="col-12 col-md-10 mb-5">
-        </div>
-        {!userParam && (
-          <div
-            className="col-12 col-md-10 mb-3 p-3"
-            style={{ border: '1px dotted #1a1a1a' }}
-          >
+        <h2>Viewing {userParam ? `${user.username}'s` : "your"} orders.</h2>
+        {user.orders.map((order) => (
+          <div key={order._id} className="order_container">
+            <h3>
+              {new Date(parseInt(order.purchaseDate)).toLocaleDateString()}
+            </h3>
+            <div className="order_info">
+              {order.products.map(({_id, image, name, price}, index) => (
+                <div key={index} className="product_info">
+                  </div>
+              ))}
+              </div>
           </div>
-        )}
+        ))}
       </div>
     </div>
   );
