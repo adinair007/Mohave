bd67bfa25a35431c765c591542f09a777134c16c
Aditya Nair <adinair.in@gmail.com>
2023-03-23 19:33:25 -0500

    Added new mobile menu to header for mobile sizes

--- client/src/components/Header/index.js
+++ client/src/components/Header/index.js
@@ -1,61 +1,57 @@
-import { useState } from "react";
-import React from "react";
+import React, { useRef, useState } from "react";
 import { Link } from "react-router-dom";
+import { FaBars, FaTimes } from "react-icons/fa";
 import Logo from "../../assets/Mojave2.png";
 import Auth from "../../utils/auth";
-import SearchIcon from "@material-ui/icons/Search";
 import CartIcon from "@material-ui/icons/ShoppingCart";
 import { useStateValue } from "../../StateProvider";
 import "./Header.css";
-import Search from "../SearchBar";
 
+export default function Header({ searchValue, searchHandler }) {
+  const logout = (event) => {
+    event.preventDefault();
+    Auth.logout();
+  };
+  const [{ cart }, dispatch] = useStateValue();
 
+  const navRef = useRef();
 
+  const toggleMobileMenu = () => {
+    navRef.current.classList.toggle("open");
+  };
 
+  // const onChange = (event) => {
+  //     setValue(event.target.value);
+  // };
 
+  // const onSearch = (searchTerm) => {
+  //     setValue(searchTerm);
+  //     console.log("search", searchTerm);
+  // };
 
+  // const dropDownStyle = data.length ? { display: 'block' } : { display: 'none' }
 
-export default function Header({searchValue, searchHandler}) {
-    const logout = (event) => {
-        event.preventDefault();
-        Auth.logout();
-    };
+  // const [keyword, setKeyword] = useState('')
 
-    // const onChange = (event) => {
-    //     setValue(event.target.value);
-    // };
+  //    const searchHandler = (e) => {
+  //     e.preventDefault()
 
-    // const onSearch = (searchTerm) => {
-    //     setValue(searchTerm);
-    //     console.log("search", searchTerm);
-    // };
-    const [{ cart }, dispatch] = useStateValue();
+  //     if(keyword.trim()) {
+  //       history.push(`/search/${keyword}`)
+  //     } else {
+  //       history.push('/')
+  //     }
+  //    }
 
-    // const dropDownStyle = data.length ? { display: 'block' } : { display: 'none' }
-
-    // const [keyword, setKeyword] = useState('')
-
-//    const searchHandler = (e) => {
-//     e.preventDefault()
-
-//     if(keyword.trim()) {
-//       history.push(`/search/${keyword}`)
-//     } else {
-//       history.push('/')
-//     }
-//    }
-
-
-
-    return (
-        <div className="header">
-          <div>
-              <Link to="/">
-                  <img className="header_logo" src={Logo} alt="Mojave Logo" />
-              </Link>
-          </div>
-          
-          <form className="header_search flex flex-col w-72">
+  return (
+    <div className="header">
+      <div>
+        <Link to="/">
+          <img className="header_logo" src={Logo} alt="Mojave Logo" />
+        </Link>
+      </div>
+      <div className="search_bar">
+        <form className="header_search">
           {/* <Search /> */}
           <input
             className="search_input"
@@ -63,192 +59,82 @@ export default function Header({searchValue, searchHandler}) {
             placeholder="Search Product"
             value={searchValue}
             onChange={(e) => searchHandler(e.target.value)}
-        />
-         </form>
-
-          
-         
-          <div className="header_nav">
-              <Link to="/login">
-                  <div className="header_option">
-                      <span className="header_optionl1">Hello {!Auth.loggedIn() ? 'Guest' : 'Friend!'}</span>
-                      <span className="header_optionl2">{!Auth.loggedIn() ? 'Sign In' : 'Sign Out'}</span>
-                  </div>
-              </Link>
-              
-              <Link to="/wishlist">
+          />
+        </form>
+      </div>
+      <nav>
+        <div className="header_nav">
+          <Link to="/login">
             <div className="header_option">
-            <span className="header_optionl1">Your</span>
-            <span className="header_optionl2">Wishlist</span>
+              <span className="header_optionl1">
+                Hello {!Auth.loggedIn() ? "Guest" : "Friend!"}
+              </span>
+              <span className="header_optionl2">
+                {!Auth.loggedIn() ? "Sign In" : "Sign Out"}
+              </span>
             </div>
-             </Link>
-              
+          </Link>
 
+          <Link to="/wishlist">
+            <div className="header_option">
+              <span className="header_optionl1">Your</span>
+              <span className="header_optionl2">Wishlist</span>
+            </div>
+          </Link>
+
+          <Link to="/orders">
+            <div className="header_option">
+              <span className="header_optionl1">Returns</span>
+              <span className="header_optionl2">& Orders</span>
+            </div>
+          </Link>
+
+          <Link to="/checkout">
+            <div className="header_optionCart">
+              <CartIcon />
+              <span className="header_optionl2 header_cartCount">
+                {cart?.length}
+              </span>
+            </div>
+          </Link>
+        </div>
+      </nav>
+      <nav ref={navRef}>
+        <div onClick={toggleMobileMenu} className="hamburger_menu">
+          <div className="bar1"></div>
+          <div className="bar2"></div>
+          <div className="bar3"></div>
+          <ul className="mobile_menu">
+            <li>
+              <Link to="/wishlist">
+                <span className="mobile_option">Wishlist</span>
+              </Link>
+            </li>
+            <li>
               <Link to="/orders">
-                  <div className="header_option">
-                      <span className="header_optionl1">Returns</span>
-                      <span className="header_optionl2">& Orders</span>
-                  </div>
+                <span className="mobile_option">Returns & Orders</span>
               </Link>
-
+            </li>
+            <li>
               <Link to="/checkout">
-                  <div className="header_optionCart">
-                      <CartIcon />
-                      <span className="header_optionl2 header_cartCount">
-                          {cart?.length}
-                      </span>
-                  </div>
+                <span className="mobile_option">
+                  <span className="cart_icon">
+                    Cart <CartIcon />
+                  <span className="header_cartCount">{cart?.length}</span>
+                </span>
+                </span>
               </Link>
-          </div>
-        </div> 
-    )
-
-};
-
-
-
-
-
-
-
-
-
-
-// import React from 'react'
-
-
-// const SearchBar = ({history}) => {
-//    const [keyword, setKeyword] = useState('')
-
-//    const searchHandler = (e) => {
-//     e.preventDefault()
-
-//     if(keyword.trim()) {
-//       history.push(`/search/${keyword}`)
-//     } else {
-//       history.push('/')
-//     }
-//    }
-  
-  
-//   return (
-//     <div>
-//      <form onSubmit={searchHandler}>
-//       <div className= "input-group">
-//         <input 
-//         type="text"
-//         id="search_field"
-//         classname="form-control"
-//         placeholder="Enter product Name..."
-//         onChange={(e) => setKeyword(e.target.value)}
-//         />
-//         <div className="input-group-append">
-//           <button id="search btn" className="btn">
-//             <i className="fa fa-search" aria-hidden="true"></i>
-//           </button>
-//         </div>
-//       </div>
-//      </form>
-//     </div>
-//   )
-// }
-
-// export default SearchBar;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import React from "react";
-// import { Link } from "react-router-dom";
-// import Logo from "../../assets/Mojave2.png";
-// import Auth from "../../utils/auth";
-// import SearchIcon from "@material-ui/icons/Search";
-// import CartIcon from "@material-ui/icons/ShoppingCart";
-// import { useStateValue } from "../../StateProvider";
-// import "./Header.css";
-
-// const Header = () => {
-//   const logout = (event) => {
-//     event.preventDefault();
-//     Auth.logout();
-//   };
-
-//   const [{ cart }, dispatch] = useStateValue();
-
-//   return (
-//     <div className="header">
-//       <div>
-//         <Link to="/">
-//           <img className="header_logo" src={Logo} alt="Mojave Logo" />
-//         </Link>
-//       </div>
-//       {/* <div className="header_search">
-//         <input className="search_input" type="text" />
-//         <SearchIcon className="header_searchIcon" />
-//       </div> */}
-
-
-
-      
-//       <div className="header_nav">
-//         <Link to="/login">
-//           <div className="header_option">
-//             <span className="header_optionl1">Hello Guest</span>
-//             <span className="header_optionl2">Sign In</span>
-//           </div>
-//         </Link>
-//         <Link to="/orders">
-//           <div className="header_option">
-//             <span className="header_optionl1">Returns</span>
-//             <span className="header_optionl2">& Orders</span>
-//           </div>
-//         </Link>
-//         <Link to="/checkout">
-//           <div className="header_optionCart">
-//             <CartIcon />
-//             <span className="header_optionl2 header_cartCount">
-//               {cart?.length}
-//             </span>
-//           </div>
-//         </Link>
-//       </div>
-//       {/* <div>
-//           {Auth.loggedIn() ? (
-//             <>
-//               <Link className="btn btn-lg btn-info m-2" to="/me">
-//                 {Auth.getProfile().data.username}'s profile
-//               </Link>
-//               <button className="btn btn-lg btn-light m-2" onClick={logout}>
-//                 Logout
-//               </button>
-//             </>
-//           ) : (
-//             <>
-//               <Link className="btn btn-lg btn-info m-2" to="/login">
-//                 Login
-//               </Link>
-//               <Link className="btn btn-lg btn-light m-2" to="/signup">
-//                 Signup
-//               </Link>
-//             </>
-//           )}
-//         </div> */}
-//     </div>
-//   );
-// };
-
-// export default Header;
+            </li>
+            <li>
+          <Link to="/login">
+            <span className="mobile_option">
+              {!Auth.loggedIn() ? "Sign In" : "Sign Out"}
+            </span>
+        </Link>
+          </li>
+          </ul>
+        </div>
+      </nav>
+    </div>
+  );
+}
